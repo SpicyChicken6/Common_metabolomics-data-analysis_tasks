94d64cfb53f56ca655665d8e45c43f73a785e507
Zhijian Yu <yuzhijian@outlook.com>
2023-03-20 22:13:40 -0400

    edit setup file

--- bin/test/unit_test/test_functions.py
+++ bin/test/unit_test/test_functions.py
@@ -1,7 +1,7 @@
-import modules.test_module as tm
-import modules.data_preprocessing.normalization as nm
-import modules.data_preprocessing.transformation as tf
-import modules.data_preprocessing.data_reading as dp
+import bin.test_module as tm
+import bin.data_preprocessing.normalization as nm
+import bin.data_preprocessing.transformation as tf
+import bin.data_preprocessing.data_reading as dp
 
 ## a simple test function to see if unit testing is working
 def test_xy_sum():
--- setup.py
+++ setup.py
@@ -1,5 +1,10 @@
 from setuptools import setup
 
-setup(name="metabolomics_analysis_tools",
-	version ="0.1.0",
-	packages=["modules"])
+setup(name='metabolomics_analysis_tools',
+      version='0.1.0',
+      description='common metabolomics analysis tools',
+      author='Zhijian Yu',
+      author_email='yuzhijian@outlook.com',
+      url='https://github.com/SpicyChicken6/metabolomics_analysis_tools.git',
+      packages=['bin'],
+     )
