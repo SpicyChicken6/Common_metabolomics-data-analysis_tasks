correct scaling function

metabolomics_analysis_tools/data_preprocessing/scaling.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-def data_scaling_mean_centered(input_data):
+def data_scaling_mean_centered(normalized_data):
     '''
     Scale the data by subtracting the mean of each column from each value in the column
 
@@ -11,8 +11,8 @@ def data_scaling_mean_centered(input_data):
         pandas.DataFrame: Scaled dataframe
     '''
 
-    data_scaled = input_data.subtract(input_data.mean(axis=0), axis=1)
-    scaled_data=input_data.copy
+    data_scaled = normalized_data.iloc[:,2:].subtract(normalized_data.mean(axis=0), axis=1)
+    scaled_data=normalized_data.copy()
     scaled_data.iloc[:,2:]=data_scaled
 
     return scaled_data